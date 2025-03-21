59efcda465913be3696dc23614648c63ea7c9a53
Kim <SKI2959@emory.edu>
2025-03-21 18:33:13 -0400

    CountsFiles_to_CountsMatrix.R update complete

--- CountsFiles_to_CountsMatrix.R
+++ CountsFiles_to_CountsMatrix.R
@@ -24,3 +24,17 @@ if (all_identical) {
 } else {
   cat("The 8 files does not have identical rows for the gene_id column\n")
 }
+
+# If gene_id columns are identical, create and export GeneCountMatrix_Original.csv
+if (all_identical) {
+  gene_count_matrix <- data.frame(gene_id = gene_id_lists[[1]])
+  sample_labels <- c("Control_R1", "Control_R2", "Control_R3", "Control_R4", 
+                     "Experimental_R1", "Experimental_R2", "Experimental_R3", "Experimental_R4")
+  
+  for (i in seq_along(file_paths)) {
+    counts <- read.table(file_paths[i], header = TRUE, sep = "\t", stringsAsFactors = FALSE)$expected_count
+    gene_count_matrix[[sample_labels[i]]] <- counts
+  }
+  
+  write.csv(gene_count_matrix, file = "../public/GeneCountMatrix_Original.csv", row.names = FALSE)
+}
